ce5b2951bde75288979aa2a77f5de339dd631f3c
Jamie Alquiza <jamiealquiza@gmail.com>
2017-07-02 09:57:07 -0600

    defaults ch to 32 vnodes

--- ch.go
+++ ch.go
@@ -7,7 +7,7 @@ import (
 var ring *ch.Ring
 
 func chInit() {
-	ring, _ = ch.New(&ch.Config{VNodes: 3})
+	ring, _ = ch.New(&ch.Config{VNodes: 32})
 
 	for _, n := range nodeList {
 		ring.AddNode(n)
